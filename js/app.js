c623724798fdcfdd6c12b9ab2c110fd2d4aa73c7
cumbinar <cumbiweb@gmail.com>
2022-04-05 15:45:05 -0500

    function declaration

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-console.log("Haola Cumbi");
+console.log("Hola Cumbi Tara");
+
+//Funciones/////
+
+/*function declaration*/
+
+function sumar() {
+    console.log(2 + 2);
+} /*se declara la función*/
+sumar(); /*se llama a la función*/
