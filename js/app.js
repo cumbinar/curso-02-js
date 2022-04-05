545c22300a5c5740a3f4becc7f459a8a8dcac096
cumbinar <cumbiweb@gmail.com>
2022-04-05 15:49:58 -0500

    function expresion

--- js/app.js
+++ js/app.js
@@ -8,3 +8,12 @@ function sumar() {
     console.log(2 + 2);
 } /*se declara la función*/
 sumar(); /*se llama a la función*/
+sumar();
+sumar(); /*se uede llamar varias veces la función, se reutiliza*/
+
+/*function expresion*/
+
+const sumar2 = function() {
+    console.log(3 + 5);
+}
+sumar2();
